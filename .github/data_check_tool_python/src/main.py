f53a3a74dd68be3b58dba85e3fed87c6999b1614
haniyeka <hkashgar@uwyo.edu>
2023-08-18 12:03:23 -0600

    test workflow on push

--- .github/data_check_tool_python/src/main.py
+++ .github/data_check_tool_python/src/main.py
@@ -33,14 +33,6 @@ class Checker(object):
         parser.add_argument("--dir",dest="dir_", required=True, help="directory path with input files")
         
         args_ = parser.parse_args(sys.argv[1:])
-        
-        # Check for temporary files starting with ".*"
-        temp_files = glob.glob(args_.dir_ + '/.*')
-        if temp_files:
-            print("Error: Temporary files (starting with a dot) found in the directory. Please remove the following files and try again:")
-            for file in temp_files:
-                print(file)
-            sys.exit(1)  # Exit with error code
 
         # dummy parameter
         args_.feat_time = -1
